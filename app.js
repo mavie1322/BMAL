9d6bb1bc85f9e879d891348f2ba17b7d0113fd78
mavie1322 <babimbilyoness@gmail.com>
2022-02-07 13:31:45 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { getSummaryEndpoints } = require('./controllers/api.controller');
-const { getTopics } = require('./controllers/topics.controller');
+const { getSummaryEndpoints } = require("./controllers/api.controller");
+const { getTopics } = require("./controllers/topics.controller");
 const {
   postCommentByArticleId,
   removeCommentById,
   getCommentsByArticleId,
   patchCommentById,
-} = require('./controllers/comments.controller');
+} = require("./controllers/comments.controller");
 const {
   getUsers,
   getUserByUsername,
-} = require('./controllers/users.controller');
+} = require("./controllers/users.controller");
 const {
   getArticleById,
   patchArticleById,
   getArticles,
-} = require('./controllers/articles.controller');
+} = require("./controllers/articles.controller");
 
 const {
   handleCustomErrors,
   handlePsqlErrors,
   handleServerErrors,
   handle404s,
-} = require('./errors/index');
+} = require("./errors/index");
 
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
-app.get('/api/topics', getTopics);
+app.get("/api/topics", getTopics);
 
-app.get('/api/users', getUsers);
-app.get('/api/users/:username', getUserByUsername);
+app.get("/api/users", getUsers);
+app.get("/api/users/:username", getUserByUsername);
 
-app.get('/api/articles', getArticles);
+app.get("/api/articles", getArticles);
 
-app.get('/api/articles/:article_id', getArticleById);
-app.patch('/api/articles/:article_id', patchArticleById);
+app.get("/api/articles/:article_id", getArticleById);
+app.patch("/api/articles/:article_id", patchArticleById);
 
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-app.post('/api/articles/:article_id/comments', postCommentByArticleId);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
-app.delete('/api/comments/:comment_id', removeCommentById);
-app.patch('/api/comments/:comment_id', patchCommentById);
+app.delete("/api/comments/:comment_id", removeCommentById);
+app.patch("/api/comments/:comment_id", patchCommentById);
 
-app.get('/api', getSummaryEndpoints);
+app.get("/api", getSummaryEndpoints);
 
 //ERROR HANDLING
-app.all('*', handle404s);
+app.all("*", handle404s);
 
 app.use(handleCustomErrors);
 
