2f864bc0c7a7a5d86a58df2430ce3d7ca8146b38
mavie1322 <babimbilyoness@gmail.com>
2022-03-09 23:49:02 +0000

    create the controller functions to remove and post article

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,12 +1,14 @@
 const {
   selectArticleById,
   updateArticleById,
+  deleteArticleById,
   selectArticles,
+  insertArticle,
 } = require("../models/articles.model");
 
 const { selectTopics } = require("../models/topics.model");
 
-const { getArticlesColumn } = require("../db/utils/index");
+const { getArticlesColumn, checkArticleIdExist } = require("../db/utils/index");
 
 exports.getArticleById = (req, res, next) => {
   const id = req.params.article_id;
@@ -34,9 +36,7 @@ exports.patchArticleById = (req, res, next) => {
         res.status(200).send({ article });
       }
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch((err) => next(err));
 };
 
 exports.getArticles = (req, res, next) => {
@@ -69,17 +69,58 @@ exports.getArticles = (req, res, next) => {
               res.status(200).send({ articles });
             })
             .catch((err) => {
-              console.log(err, "<<<1");
               next(err);
             });
         })
         .catch((err) => {
-          console.log(err, "<<<2");
           next(err);
         });
     })
     .catch((err) => {
-      console.log(err, "<<<3");
       next(err);
     });
+};
+
+exports.postArticles = (req, res, next) => {
+  console.log(req.body);
+  const reqBody = req.body;
+  const requestProperty = Object.keys(reqBody);
+  const reqProperty = ["body", "author", "title", "topic"];
+
+  //Checking if request body property is missing field
+  if (requestProperty.length !== reqProperty.length) {
+    next({ status: 400, msg: "Bad Request" });
+  }
+
+  //Checking if there is any mispelled on the property
+  const isMispelled = requestProperty.map((property) => {
+    return reqProperty.includes(property);
+  });
+  if (isMispelled.includes(false)) {
+    next({ status: 400, msg: "Bad Request" });
+  }
+  insertArticle(reqBody)
+    .then((article) => {
+      res.status(201).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.removeArticlesById = (req, res, next) => {
+  const { article_id } = req.params;
+  return checkArticleIdExist(article_id)
+    .then((isArticleExist) => {
+      if (isArticleExist) {
+        deleteArticleById(article_id)
+          .then((result) => {
+            res.status(204).end();
+          })
+          .catch((err) => next(err));
+      } else {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      }
+    })
+    .catch((err) => next(err));
 };
